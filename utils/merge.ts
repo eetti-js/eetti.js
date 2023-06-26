5abb683fdc641b6801e43173c4dfc829b4b705f0
rifkan <mohamedrifkan00127@gmail.com>
2023-06-26 23:49:00 +0530

    fixe

--- utils/merge.ts
+++ utils/merge.ts
@@ -11,15 +11,19 @@
 
 const hasOwnProperty = Object.prototype.hasOwnProperty;
 
-function merge<T extends object, U extends object>(dest: T, src: U, override = true): T & U {
-  if (!dest) throw new TypeError('argument dest is required');
-  if (!src) throw new TypeError('argument src is required');
+function merge<T extends object, U extends object>(
+  dest: T,
+  src: U,
+  override = true
+): T & U {
+  if (!dest) throw new TypeError("argument dest is required");
+  if (!src) throw new TypeError("argument src is required");
 
   Object.keys(src).forEach((key) => {
     if (!override && hasOwnProperty.call(dest, key)) return;
 
     const descriptor = Object.getOwnPropertyDescriptor(src, key);
-    Object.defineProperty(dest, key, descriptor);
+    Object.defineProperty(dest, key, descriptor!);
   });
 
   return dest as T & U;
